cc9a7e87bfbeb563db1ff3a69c6c38f6b9b76d8c
mimihimesama <gwa446@gmail.com>
2024-05-16 10:33:39 +0900

    ✨ update : 루트 경로에 대한 처리 추가, .env 관리 수정

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 import express from "express";
 import connect from "./schemas/index.js";
-import CharacterRouter from "./routes/characters.router.js";
-import dotenv from "dotenv";
+import charactersRouter from "./routes/characters.router.js";
 
 const app = express();
 const PORT = 3000;
 
-dotenv.config();
 connect();
 
 // Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// 루트 경로에 대한 처리 추가
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the root!" });
+});
 
 const router = express.Router();
 
@@ -19,7 +22,7 @@ router.get("/", (req, res) => {
   return res.json({ message: "Hi!" });
 });
 
-app.use("/api", [router, CharacterRouter]);
+app.use("/api", [router, charactersRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
