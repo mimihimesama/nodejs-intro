2f246b512ad8232a8ccc635a842fdfcb06e28945
mimihimesama <gwa446@gmail.com>
2024-05-17 15:59:22 +0900

    🩹 Fix : error-handler 코드 추가

--- middlewares/error-handler.middleware.js
+++ middlewares/error-handler.middleware.js
@@ -6,8 +6,10 @@ export default function (err, req, res, next) {
     return res.status(400).json({ errorMessage: err.message });
   }
 
+  if (item_code === "ValidationError" || item_name === "ValidationError" || item_stat === "ValidationError") {
+    return res.status(400).json({ errorMessage: err.message });
+  }
+
   // 그 외의 에러가 발생하면, 서버 에러로 처리합니다.
-  return res
-    .status(500)
-    .json({ errorMessage: "서버에서 에러가 발생하였습니다." });
+  return res.status(500).json({ errorMessage: "서버에서 에러가 발생하였습니다." });
 }
