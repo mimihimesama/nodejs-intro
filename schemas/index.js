7ee211939d71b3d9642d21ae4c5cfda1e84b6589
mimihimesama <gwa446@gmail.com>
2024-05-17 16:51:04 +0900

    🔒️ Fix : .env

--- schemas/index.js
+++ schemas/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connect = () => {
   mongoose
     .connect(process.env.MONGODB_URI, {
-      dbName: "node_lv1",
+      dbName: process.env.MONGODB_DB_NAME,
     })
     .then(() => console.log("MongoDB 연결에 성공하였습니다."))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
